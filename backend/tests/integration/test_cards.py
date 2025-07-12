3900af2f89764330273b99dced5cbe5563937568
Elias Carlson <eliaskjellbergcarlson@gmail.com>
2025-07-12 23:16:45 +0200

    update tests

--- backend/tests/integration/test_cards.py
+++ backend/tests/integration/test_cards.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+from freezegun import freeze_time
 
 from src.db.models import Card
 from tests.asserts import is_utc_isoformat_string, is_uuid_string
@@ -26,8 +27,10 @@ async def test_create_card_returns_card(db_session, user, user_client):
     assert res.json() == {
         "id": is_uuid_string(),
         "deck_id": deck_id,
+        "deck_name": "deck",
         "content": "Test card",
         "next_review_date": is_utc_isoformat_string(),
+        "overdue": False,
         "created_at": is_utc_isoformat_string(),
         "updated_at": is_utc_isoformat_string(),
     }
@@ -94,40 +97,50 @@ async def test_get_cards(user, user_client):
         {
             "id": is_uuid_string(),
             "deck_id": deck_id,
+            "deck_name": "deck",
             "content": "Test card 1",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
         {
             "id": is_uuid_string(),
             "deck_id": deck_id,
+            "deck_name": "deck",
             "content": "Test card 2",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
         {
             "id": is_uuid_string(),
             "deck_id": deck_id,
+            "deck_name": "deck",
             "content": "Test card 3",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
         {
             "id": is_uuid_string(),
             "deck_id": deck_id,
+            "deck_name": "deck",
             "content": "Test card 4",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
         {
             "id": is_uuid_string(),
             "deck_id": deck_id,
+            "deck_name": "deck",
             "content": "Test card 5",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
@@ -175,8 +188,10 @@ async def test_get_cards_by_deck(db_session, user, user_client):
         {
             "id": card_1_id,
             "deck_id": deck_1_id,
+            "deck_name": "deck",
             "content": "Test card",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
@@ -226,16 +241,20 @@ async def test_get_due_cards(db_session, user, user_client):
         {
             "id": card_1_id,
             "deck_id": deck_1_id,
+            "deck_name": "deck",
             "content": "Test card 1",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
         {
             "id": card_2_id,
             "deck_id": deck_2_id,
+            "deck_name": "deck",
             "content": "Test card 2",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
@@ -251,8 +270,10 @@ async def test_get_due_cards(db_session, user, user_client):
         {
             "id": card_2_id,
             "deck_id": deck_2_id,
+            "deck_name": "deck",
             "content": "Test card 2",
             "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
             "created_at": is_utc_isoformat_string(),
             "updated_at": is_utc_isoformat_string(),
         },
@@ -265,6 +286,58 @@ async def test_get_due_cards(db_session, user, user_client):
 
     res = await user_client.get("/cards", params={"only_due": True})
     assert res.json() == []
+
+
+@freeze_time("2025-07-12 12:00:00")
+async def test_get_overdue_cards(ignore_jwt_expiration, db_session, user, user_client):
+    res = await user_client.post(
+        "/decks",
+        json={
+            "name": "deck",
+            "description": "my deck",
+        },
+    )
+    deck_id = res.json()["id"]
+
+    res = await user_client.post(
+        "/cards",
+        json={
+            "deck_id": deck_id,
+            "content": "Test card",
+        },
+    )
+    card_id = res.json()["id"]
+
+    # Card should be due but not overdue when just created
+    res = await user_client.get("/cards", params={"only_due": True})
+    assert res.json() == [
+        {
+            "id": card_id,
+            "deck_id": deck_id,
+            "deck_name": "deck",
+            "content": "Test card",
+            "next_review_date": is_utc_isoformat_string(),
+            "overdue": False,
+            "created_at": is_utc_isoformat_string(),
+            "updated_at": is_utc_isoformat_string(),
+        },
+    ]
+
+    # Move time forward 1 day, card should now be overdue
+    with freeze_time("2025-07-13 12:00:00"):
+        res = await user_client.get("/cards", params={"only_due": True})
+        assert res.json() == [
+            {
+                "id": card_id,
+                "deck_id": deck_id,
+                "deck_name": "deck",
+                "content": "Test card",
+                "next_review_date": is_utc_isoformat_string(),
+                "overdue": True,
+                "created_at": is_utc_isoformat_string(),
+                "updated_at": is_utc_isoformat_string(),
+            },
+        ]
 
 
 async def test_update_card(user, user_client):
@@ -294,8 +367,10 @@ async def test_update_card(user, user_client):
     assert res.json() == {
         "id": is_uuid_string(),
         "deck_id": deck_id,
+        "deck_name": "deck",
         "content": "Updated card",
         "next_review_date": is_utc_isoformat_string(),
+        "overdue": False,
         "created_at": is_utc_isoformat_string(),
         "updated_at": is_utc_isoformat_string(),
     }
