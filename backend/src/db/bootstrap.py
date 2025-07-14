6a577a31238879ff64f7098458643039be1bfc43
Erik Wessman <erikwessman10@gmail.com>
2025-07-14 14:13:24 +0200

    Add decks with content and a regular user

--- backend/src/db/bootstrap.py
+++ backend/src/db/bootstrap.py
@@ -1,32 +1,32 @@
-import bcrypt
+from sqlalchemy.orm import Session
 
 from src.db import get_db
 from src.db.models import User, UserRole
 from src.import_export import BaseImporter, store_imported_deck
 from src.import_export.custom import CustomImporter
 
-DECK_JSON_PATH = "data/deck.json"
+FRENCH_DECK_JSON_PATH = "data/french.json"
+CHINESE_DECK_JSON_PATH = "data/chinese.json"
+
+
+def add_user(email: str, password: str, role: UserRole, db_session: Session) -> User:
+    user = User(email=email, role=UserRole.ADMIN)
+    user.set_password(password)
+    user = user.save(db_session)
+    return user
 
 
 def bootstrap():
     with next(get_db()) as db_session:
-        email = "admin@domain.com"
-        password = "password"
+        add_user("admin@domain.com", "password", UserRole.ADMIN, db_session)
+        user = add_user("user@domain.com", "password", UserRole.USER, db_session)
 
-        pw_bytes = password.encode("utf-8")
-        pw_hashed = bcrypt.hashpw(pw_bytes, bcrypt.gensalt()).decode("utf-8")
-        user = User(email=email, password_hash=pw_hashed, role=UserRole.ADMIN)
-        user = user.save(db_session)
-
-        print(f"Bootstrapped {email}")
-
-        with open(DECK_JSON_PATH, "rb") as file:
-            content = file.read()
-            importer: BaseImporter = CustomImporter()
-            deck_data = importer.parse(content)
-            store_imported_deck(deck_data, user.id, db_session)
-
-        print(f"Bootstrapped {DECK_JSON_PATH}")
+        for deck_path in {FRENCH_DECK_JSON_PATH, CHINESE_DECK_JSON_PATH}:
+            with open(deck_path, "rb") as file:
+                content = file.read()
+                importer: BaseImporter = CustomImporter()
+                deck_data = importer.parse(content)
+                store_imported_deck(deck_data, user.id, db_session)
 
 
 if __name__ == "__main__":
