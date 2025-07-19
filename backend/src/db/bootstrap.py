ca33be12c7bfa69e97578fb292e43df0b283d35f
Erik Wessman <erikwessman10@gmail.com>
2025-07-19 15:11:17 +0200

    Allow store_imported_deck to take a category_id and add a category to bootstrapped data

--- backend/src/db/bootstrap.py
+++ backend/src/db/bootstrap.py
@@ -1,7 +1,9 @@
+from uuid import UUID
+
 from sqlalchemy.orm import Session
 
 from src.db import get_db
-from src.db.models import User, UserRole
+from src.db.models import Category, User, UserRole
 from src.import_export import BaseImporter, store_imported_deck
 from src.import_export.custom import CustomImporter
 
@@ -16,17 +18,27 @@ def add_user(email: str, password: str, role: UserRole, db_session: Session) -> 
     return user
 
 
+def add_category(user_id: UUID, name: str, db_session: Session) -> Category:
+    category = Category(user_id=user_id, name=name)
+    category = category.save(db_session)
+    return category
+
+
 def bootstrap():
     with next(get_db()) as db_session:
         add_user("admin@domain.com", "password", UserRole.ADMIN, db_session)
         user = add_user("user@domain.com", "password", UserRole.USER, db_session)
+
+        language_category = add_category(user.id, "Languages", db_session)
 
         for deck_path in {FRENCH_DECK_JSON_PATH, CHINESE_DECK_JSON_PATH}:
             with open(deck_path, "rb") as file:
                 content = file.read()
                 importer: BaseImporter = CustomImporter()
                 deck_data = importer.parse(content)
-                store_imported_deck(deck_data, user.id, db_session)
+                store_imported_deck(
+                    deck_data, user.id, db_session, language_category.id
+                )
 
 
 if __name__ == "__main__":
