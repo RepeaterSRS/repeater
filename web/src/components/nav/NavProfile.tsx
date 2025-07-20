d4a45f58b7568c60e8fd9d01e6e6b1931bad22f7
Elias Carlson <eliaskjellbergcarlson@gmail.com>
2025-07-20 09:45:26 +0200

    chore: format

--- web/src/components/nav/NavProfile.tsx
+++ web/src/components/nav/NavProfile.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { toast } from 'sonner';
 
 import { ThemeChangerItems } from '@/components/ThemeChangerItems';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import {
     DropdownMenu,
     DropdownMenuContent,
