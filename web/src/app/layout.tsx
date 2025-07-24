168c692f95b44f833f3aeeabdc3e8584528de083
Erik Wessman <erikwessman10@gmail.com>
2025-07-24 13:40:21 +0200

    Format

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
-import { Geist, Geist_Mono } from 'next/font/google';
 import { PublicEnvScript } from 'next-runtime-env';
+import { Geist, Geist_Mono } from 'next/font/google';
 
 import './globals.css';
 import { AppSidebar } from '@/components/nav/AppSidebar';
